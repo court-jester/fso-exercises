b507f366f2914f35729109a219cbd9f2668634bd
Court Jester <cryptodev1@protonmail.com>
2020-08-24 20:44:38 +0200

    Unicafe step2 (1.7)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,20 +2,14 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-// Only using App component
-// const Button = ({handleClick, text}) => {
-//   return (
-//     <button onClick={handleClick}>
-//       {text}
-//     </button>
-//   )
-// }
-
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
@@ -38,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
