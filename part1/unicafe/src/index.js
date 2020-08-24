2a26f946733df685add05cc1fba8376956a06ead
Court Jester <cryptodev1@protonmail.com>
2020-08-24 21:21:35 +0200

    Unicafe step3 (1.8)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,42 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
+      // <p>all {total}</p>
+      // <p>average {average}</p>
+      // <p>positive {positive}%</p>
+
+const Statistics = ({stats}) => {
+  const {good, neutral, bad } = stats
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = (good - bad) / total
-  const positive = (good / total) * 100
+  // const total = good + neutral + bad
+  // const average = (good - bad) / total
+  // const positive = (good / total) * 100
+  const stats = {
+    good: good,
+    neutral: neutral,
+    bad: bad
+  }
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
@@ -27,14 +54,7 @@ const App = () => {
       <button onClick={increaseBad}>
         bad
       </button>
-
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <Statistics stats={stats} />
     </div>
   )
 }
