6f66aa1a7c0c05fcbc965328d358cfc424d0b02f
Court Jester <cryptodev1@protonmail.com>
2020-08-27 21:49:22 +0200

    Course information step9 (2.4)

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 
 const Course = ({ course }) => {
   return (
-    <>
+    <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </>
+    </div>
   )
 }
 
@@ -45,34 +45,58 @@ const Total = ({ parts }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ] 
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map((course) => 
+        <Course key={course.id} course={course} />
+      )}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
