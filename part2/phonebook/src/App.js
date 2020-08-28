eccb2c8ffd6bfd76f8d84ed21e775bf21a7aa8ed
Court Jester <cryptodev1@protonmail.com>
2020-08-28 19:31:59 +0200

    2.7: The phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
@@ -13,11 +13,18 @@ function App() {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName
+    const personExists = persons.some(person => person.name === newName)
+
+    if (personExists) {
+      alert(`${newName} is already added to the phonebook`)
+    } else {
+      const personObject = {
+        id: persons.length + 1,
+        name: newName
+      }
+      setPersons(persons.concat(personObject))
+      setNewName('')
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
   }
 
   return (
@@ -33,7 +40,7 @@ function App() {
       </form>
       <h2>Numbers</h2>
       {persons.map(person => 
-        <div key={person.name}>{person.name}</div>
+        <div key={person.id}>{person.name}</div>
       )}
     </div>
   )
