6816b8eae5ba6d77782e40c1faec7dc552eee228
Court Jester <cryptodev1@protonmail.com>
2020-09-05 16:00:15 +0200

    3.3: Phonebook backend step3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,6 +35,16 @@ app.get('/info', (req, res) => {
   \n${new Date}`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if  (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
 const PORT = 3001
 
 app.listen(PORT, () => {
