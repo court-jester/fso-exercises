1305eb46b4318e2c6b198f3f72e5fac57c92d1d3
Court Jester <cryptodev1@protonmail.com>
2020-09-05 16:21:46 +0200

    3.4: Phonebook backend step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
   {
     "name": "Arto Hellas",
     "number": "040-123456",
@@ -38,13 +38,21 @@ app.get('/info', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
-  
+
   if  (person) {
     res.json(person)
   } else {
     res.status(404).end()
   }
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
