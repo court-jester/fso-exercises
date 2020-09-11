daa8a638b4e55d5a9e701d573dc862d20d9deee0
Court Jester <cryptodev1@protonmail.com>
2020-09-11 21:08:09 +0200

    uibuild

---
+++ part3/phonebook/build/precache-manifest.911ecc2f3d980a2f6775089eaf44523a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cde1d35bd04b391d76dc44ed2cca058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "484f0a60a60d637a4ce2",
+    "url": "/static/css/main.4e058839.chunk.css"
+  },
+  {
+    "revision": "b7b8ab3e5c81996313f4",
+    "url": "/static/js/2.ed2fd44f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ed2fd44f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484f0a60a60d637a4ce2",
+    "url": "/static/js/main.10c162f5.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
