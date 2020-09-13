d43ac2d93492405c8c25c72a6226c17806c6b8aa
Court Jester <cryptodev1@protonmail.com>
2020-09-13 14:05:19 +0200

    3.13: Phonebook database, step1

---
+++ part3/phonebook/models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch(e => {
+    console.log('error connecting to MongoDB', e.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    console.log('returnedObject at personSchema', returnedObject);
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model('Person', personSchema);
