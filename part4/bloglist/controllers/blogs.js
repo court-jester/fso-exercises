dd0ab88ec61efbe6e16a9de9464249c167c104bd
Court Jester <cryptodev1@protonmail.com>
2020-09-23 19:14:47 +0200

    Structure the app and add endpoints

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,65 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+blogsRouter.get('/', (req, res) => {
+  Blog.find({}).then(blogs => {
+    res.json(blogs);
+  });
+});
+
+blogsRouter.get('/:id', (req, res, next) => {
+  Blog.findById(req.params.id)
+    .then(blog => {
+      if (blog) {
+        res.json(blog);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(e => next(e));
+});
+
+blogsRouter.post('/', (req, res, next) => {
+  const body = req.body;
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  });
+
+  blog
+    .save()
+    .then(savedBlog => res.status(201).json(savedBlog))
+    .catch(e => next(e));
+});
+
+blogsRouter.delete('/:id', (req, res, next) => {
+  Blog.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(e => next(e));
+});
+
+blogsRouter.put('/:id', (req, res, next) => {
+  const body = req.body;
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+    runValidators: true,
+    context: true
+  })
+    .then(updatedBlog => {
+      res.json(updatedBlog);
+    })
+    .catch(e => next(e));
+});
+
+module.exports = blogsRouter;
