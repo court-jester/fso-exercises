5095e33a3bd6d86a83a7c179f84df40a84e5b1fc
Court Jester <cryptodev1@protonmail.com>
2020-09-24 20:14:40 +0200

    Find out which blog has most likes and test it (4.5)

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = blogs => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = blogs => {
+  /** To just get the likes of the most liked blog
+   const mostLiked = Math.max(...blogs.map(blog => blog.likes));
+  */
+  const reducer = (mostLiked, item) => {
+    return mostLiked.likes > item.likes ? mostLiked : item;
+  };
+  const mostLikedBlog = blogs.reduce(reducer, 0);
+  // Format the mostLikedBlog
+  delete mostLikedBlog.url;
+  delete mostLikedBlog._id;
+  delete mostLikedBlog.__v;
+
+  return blogs.length === 0 ? [] : mostLikedBlog;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
