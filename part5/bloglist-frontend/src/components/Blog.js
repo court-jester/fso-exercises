0e8b123ce4dc624349856d1d97369f62928749fa
Court Jester <cryptodev1@protonmail.com>
2020-10-30 20:02:19 +0100

    Add toggleable details about each blog (5.7)

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [blogVisible, setBlogVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const listStyle = {
+    listStyleType: 'none',
+    padding: 0,
+    margin: 0,
+    display: blogVisible ? '' : 'none'
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => setBlogVisible(true)}>view</button>
+      <ul style={listStyle}>
+        <li>{blog.url}</li>
+        <li>
+          likes {blog.likes} <button>like</button>
+        </li>
+        <li>{blog.author}</li>
+        <button onClick={() => setBlogVisible(false)}>close</button>
+      </ul>
+    </div>
+  );
+};
 
 Blog.propTypes = {
   blog: PropTypes.object
